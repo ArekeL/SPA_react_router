8838c30bb210418c93db588cf9825a9149225ffd
Arek Lechnik <alechnik@gmail.com>
2023-01-11 16:27:44 +0100

    Create navigation + css

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,5 +1,23 @@
+import { NavLink } from "react-router-dom";
+import "../styles/Navigation.css";
+
+const menuList = [
+	{ name: "home", path: "/" },
+	{ name: "articles", path: "/articles" },
+	{ name: "contact", path: "/contact" },
+	{ name: "panel", path: "/panel" },
+];
 const Navigaton = () => {
-	return <div>Navigation</div>;
+	const menu = menuList.map((item) => (
+		<li key={item.name}>
+			<NavLink to={item.path}>{item.name.toUpperCase()}</NavLink>
+		</li>
+	));
+	return (
+		<nav className='main'>
+			<ul>{menu}</ul>
+		</nav>
+	);
 };
 
 export default Navigaton;
