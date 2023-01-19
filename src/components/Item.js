128eb256568d1da73f15a2c2b42b2540117f04f4
Arek Lechnik <alechnik@gmail.com>
2023-01-19 06:54:46 +0100

    Footer + css

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/HomePage.css";
+
 const Item = () => {
 	return (
 		<article className='home'>
--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import "../styles/Footer.css";
+
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className='footer'>
+			<h2>Footer</h2>
+			<Link to='/'>
+				<p>Home</p>
+			</Link>
+			<Link to='/item/:id'>
+				<p>About Us</p>
+			</Link>
+			<Link to='/contact'>
+				<p>Contact</p>
+			</Link>
+		</div>
+	);
 };
 
 export default Footer;
