80614535e69250f23312242ecef22691acd55654
Arek Lechnik <alechnik@gmail.com>
2023-01-23 06:40:20 +0100

    Add - basename to App.js

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 	state = {};
 	render() {
 		return (
-			<Router>
+			<Router basename={process.env.PUBLIC_URL}>
 				<div className='app'>
 					<header>{<Header />}</header>
 					<main>
